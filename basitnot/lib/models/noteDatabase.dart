9fec2d47e638624cb36b1aa611f9ebbcbe3af024
HajordaM <hajorda@gmail.com>
2024-02-09 16:53:28 +0300

    Isar setup, Note schema generated

---
+++ basitnot/lib/models/noteDatabase.dart
@@ -0,0 +1,17 @@
+class NoteDatabase{
+ 
+  //Initialize the database
+
+  //Create a new note
+
+  //Update a note
+
+  //Delete a note
+
+  //Retrieve all notes
+
+  //Retrieve a single note
+
+  //Delete all notes
+
+}
---
+++ basitnot/lib/pages/notesPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class notesPage extends StatefulWidget {
+  const notesPage({super.key});
+
+  @override
+  State<notesPage> createState() => _notesPageState();
+}
+
+class _notesPageState extends State<notesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
